0da1ad322933725105a9ef45ac3789404d4b95b4
Mayerli2004 <128087546+Mayerli2004@users.noreply.github.com>
2023-03-24 14:00:05 -0500

    Update contract.blade.php

---
+++ Modules/TH/Resources/views/contract.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+	
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+		
+		<!-- Poper Bootstrap --> 
+	    
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" href="https://kit.fontawesome.com/dcb1bbced2.css" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/dcb1bbced2.js" crossorigin="anonymous"></script>
+
+
+
+
+
+</head>
+<center>
+	<br>
+<ht colspan="5"><h1> REGISTROS DE CONTRATO </h1></th>;
+</center>
+<form action="" method="post">
+        <center>
+        <label> Numero de documento</label>
+        <input type="number" name="documento" class="form-control" style="width: 65%"  placeholder="Numero ID"><br>
+        <label>Nombres y apellidos</label>
+        <input type="text" name="nombre"  class="form-control" style="width: 65%" placeholder="Nombres"><br>
+
+        
+        </select><br>
+        
+		<input type="submit" name="btn_guardar" class="btn btn-success" value="consulta">
+	
+		<td><button type="button" class="btn btn-success editbtn" data-bs-toggle="modal" data-bs-target="#editar"><i class="fa-solid fa-file-pdf"></i></button></td>
+      
+        
+          <br>
+          <br>
+              </center>
