3db6e257d8f7a2fcf3286ba650e357a8d7ba7d3d
C4DENA <acadenacuellar2002@gmail.com>
2023-03-24 13:10:30 -0500

    vistas unidades and links in layaouts master :)

--- Modules/AGROINDUSTRIA/Resources/views/index.blade.php
+++ Modules/AGROINDUSTRIA/Resources/views/index.blade.php
@@ -4,11 +4,6 @@
     
     
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('cssagroindustria/styleindex.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <nav class="navbar navbar-expand-sm navbar-Dark" style="background-color:white;">
         <a class="navbar-brand" href="#">K I R B I</a>
