d80febeaf3805f1f5163f1ba56655cbd06a4eea7
Julianchiki <julianjavierramirezdiaz73@gmail.com>
2023-03-24 14:42:30 -0500

    Correcion de navbar duplicado

--- Modules/AGROINDUSTRIA/Resources/views/unidd.blade.php
+++ Modules/AGROINDUSTRIA/Resources/views/unidd.blade.php
@@ -4,28 +4,7 @@
     
     
 
-    
 
-<!-- nav -->
-<nav class="navbar navbar-expand-sm navbar-Dark" style="background-color:white;">
-        <a class="navbar-brand" href="#">K I R B I</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">Inicio <span class="visually-hidden">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Unidades</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">movimientos</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-<!-- /nav -->
 
 
 <!-- tittle -->
