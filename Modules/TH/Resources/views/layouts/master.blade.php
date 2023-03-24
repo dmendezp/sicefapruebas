d598f132ddbcf7310af86d4f95bbc1e59abf67bf
Jhordix <jordansalazar1405@gmail.com>
2023-03-24 10:04:55 -0500

    add layout

--- Modules/TH/Resources/views/layouts/master.blade.php
+++ Modules/TH/Resources/views/layouts/master.blade.php
@@ -4,14 +4,26 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module TH</title>
+       
+        <title>Human talent</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
        {{-- Laravel Mix - CSS File --}}
        {{-- <link rel="stylesheet" href="{{ mix('css/th.css') }}"> --}}
 
     </head>
-    <body>
-        @yield('content')
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
+
+            <div class="container">
+                @yield('content')
+            </div>
 
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/th.js') }}"></script> --}}
