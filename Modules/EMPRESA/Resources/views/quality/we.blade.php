73cfa025ef31bdd27314f16943fdea5d49b1a7f8
Junior5807 <jsmedina5873@gmail.com>
2023-03-24 14:25:25 -0500

    Columnas

--- Modules/EMPRESA/Resources/views/quality/we.blade.php
+++ Modules/EMPRESA/Resources/views/quality/we.blade.php
@@ -7,67 +7,70 @@
           <h2 class="display-8">CENTRO DE FORMACIÓN AGROINDUSTRIAL "LA ANGOSTURA"!</h2>
           <p class="lead">El Centro de Formación Agroindustrial del Huila tiene como función principal impartir formación profesional integral con calidad a todos los Colombianos; con el fin de desarrollar competencias laborales que permitan la inserción laboral del personal no vinculado en el sector productivo y la cualificación del talento humano en las empresas. De igual forma articula entidades y procesos para contribuir con el desarrollo económico de la Región.</p>
         </div><br>
-        <br>
-  
-
-      <div class="grid text-center">
-    <div class="row justify-content-auto">
-        <div class="card" style="width: 20rem;">
-            <img src="https://agriculturadelasamericas.com/wp-content/uploads/2019/07/tecnologia-agricola.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title"> Tecnologo en Producción Agrícola</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div><br>
-                    <br>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://agriculturadelasamericas.com/wp-content/uploads/2019/07/tecnologia-agricola.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                            <h5 class="card-title"> Tecnologo en Producción Agrícola</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
-                  <div class="card" style="width: 20rem;">
-                    <img src="https://iescinoc.edu.co/wp-content/uploads/14144-copia-1-1199x799.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title">Tecnologo en Gestión de Empresas Agropecuarias</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://iescinoc.edu.co/wp-content/uploads/14144-copia-1-1199x799.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                            <h5 class="card-title">Tecnologo en Gestión de Empresas Agropecuarias</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                  </div>
-                  <div class="card" style="width: 20rem;">
-                    <img src="https://www.bioaquafloc.com/wp-content/uploads/2021/09/Tecnologi%CC%81a-simbio%CC%81tica-en-acuicultura-1024x768.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title">Tecnologo en Acuicultura</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
                 </div>
-            </div><br>
-            <br>
-            <br>
-            <div class="grid text-center">
-    <div class="row justify-content-auto">
-        <div class="card" style="width: 20rem;">
-            <img src="https://infoamazonia.org/wp-content/uploads/2016/02/aa-peru%CC%81-2011-2015.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title"> Tecnologo en Control Ambiental</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div><br>
-                    <br>
-                </div>
-                  <div class="card" style="width: 20rem;">
-                    <img src="https://universidadeuropea.com/resources/media/images/control-calidad-alimentos-1200x630.original.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title">Tecnologo en Control de Calidad de Alimentos</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://www.bioaquafloc.com/wp-content/uploads/2021/09/Tecnologi%CC%81a-simbio%CC%81tica-en-acuicultura-1024x768.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                            <h5 class="card-title">Tecnologo en Acuicultura</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                  </div>
-                  <div class="card" style="width: 20rem;">
-                    <img src="https://www.traza.net/wp-content/uploads/2022/02/control_de_calidad_de_alimentos.jpg" class="card-img-top" alt="..." height="348" width="1000">
-                    <div class="card-body">
-                      <h5 class="card-title">Tecnologo en Gestión de Mercados</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://infoamazonia.org/wp-content/uploads/2016/02/aa-peru%CC%81-2011-2015.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                            <h5 class="card-title"> Tecnologo en Control Ambiental</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://universidadeuropea.com/resources/media/images/control-calidad-alimentos-1200x630.original.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                            <h5 class="card-title">Tecnologo en Control de Calidad de Alimentos</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 20rem; height: 35rem;">
+                        <img src="https://www.traza.net/wp-content/uploads/2022/02/control_de_calidad_de_alimentos.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                        <div class="card-body">
+                          <h5 class="card-title">Tecnologo en Gestión de Mercados</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
 @endsection
