4ab1f2808c46a487d10e38194cd0fcd21e678819
Diego Penagos Ninco <diegopenagos955@gmail.com>
2023-03-24 14:02:15 -0500

    modificacion de contactos

--- Modules/EMPRESA/Resources/views/quality/home.blade.php
+++ Modules/EMPRESA/Resources/views/quality/home.blade.php
@@ -1,53 +1,27 @@
 @extends('empresa::layouts.adminlte')
 
 @section('content')
-    <h1>Hello World</h1>
     <br>
-    <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="https://www.sena.edu.co/es-co/regionales/zonaAndina/PublishingImages/huila/SENA_Centro_de_Formaci%C3%B3n_Agroindustrial_(Centro_Agropecuario_la_Angostura).png"class="d-block w-100" alt="..." width="600" height="800">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('empresa/img/senaempresa1.png') }}" class="d-block w-100" alt="..."  width="600" height="800">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://2.bp.blogspot.com/-TprfGeef2qU/VgshwE_N9TI/AAAAAAAAAMY/WV0lx3167zU/s1600/centro.jpg" class="d-block w-100" alt="..."  width="600" height="800">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-      </div>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://1.bp.blogspot.com/-EqlYIzbIp0U/WiF7dAYlt3I/AAAAAAAAB2M/KlQcgSxOiHYm3YGZig1ljzUGOCJ2w6e7gCLcBGAs/s1600/Aviso%2BModelo%2BFinal.jpg" class="d-block w-100" alt="...">
     </div>
-          </div>
+    <div class="carousel-item">
+    <img src="https://1.bp.blogspot.com/-EqlYIzbIp0U/WiF7dAYlt3I/AAAAAAAAB2M/KlQcgSxOiHYm3YGZig1ljzUGOCJ2w6e7gCLcBGAs/s1600/Aviso%2BModelo%2BFinal.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.sena.edu.co/es-co/Noticias/NoticiasImg/BPganaderia_13Dic2018.jpg" class="d-block w-100" alt="100">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
           <br>
 @endsection
--- Modules/EMPRESA/Resources/views/quality/we.blade.php
+++ Modules/EMPRESA/Resources/views/quality/we.blade.php
@@ -1,38 +1,69 @@
 @extends('empresa::layouts.adminlte')
 
 @section('breadcrumb')
-<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-<li class="breadcrumb-item active">Nosotros</a></li>
-@endsection
-@section('content')
-<div class="container">
-    <div class="jumbotron jumbotron-fluid">
+
+
         <div class="container">
-          <h1 class="display-4">Lo Que Ofrecemos!</h1>
-          <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </div>
-      </div><br>
+          <h2 class="display-8">CENTRO DE FORMACIÓN AGROINDUSTRIAL "LA ANGOSTURA"!</h2>
+          <p class="lead">El Centro de Formación Agroindustrial del Huila tiene como función principal impartir formación profesional integral con calidad a todos los Colombianos; con el fin de desarrollar competencias laborales que permitan la inserción laboral del personal no vinculado en el sector productivo y la cualificación del talento humano en las empresas. De igual forma articula entidades y procesos para contribuir con el desarrollo económico de la Región.</p>
+        </div><br>
+        <br>
+  
+
+      <div class="grid text-center">
     <div class="row justify-content-auto">
         <div class="card" style="width: 20rem;">
-            <img src="https://static.wixstatic.com/media/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png/v1/fill/w_387,h_441,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png" class="card-img-top" alt="..." height="348" width="1000">
+            <img src="https://agriculturadelasamericas.com/wp-content/uploads/2019/07/tecnologia-agricola.jpg" class="card-img-top" alt="..." height="348" width="1000">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title"> Tecnologo en Producción Agrícola</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div><br>
+                    <br>
                 </div>
                   <div class="card" style="width: 20rem;">
-                    <img src="https://static.wixstatic.com/media/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png/v1/fill/w_387,h_441,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png" class="card-img-top" alt="..." height="348" width="1000">
+                    <img src="https://iescinoc.edu.co/wp-content/uploads/14144-copia-1-1199x799.jpg" class="card-img-top" alt="..." height="348" width="1000">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title">Tecnologo en Gestión de Empresas Agropecuarias</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                   </div>
                   <div class="card" style="width: 20rem;">
-                    <img src="https://static.wixstatic.com/media/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png/v1/fill/w_387,h_441,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/30d63c_bb9cb5514b0546beafb671ec02cd2244~mv2.png" class="card-img-top" alt="..." height="348" width="1000">
+                    <img src="https://www.bioaquafloc.com/wp-content/uploads/2021/09/Tecnologi%CC%81a-simbio%CC%81tica-en-acuicultura-1024x768.jpg" class="card-img-top" alt="..." height="348" width="1000">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title">Tecnologo en Acuicultura</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+            </div><br>
+            <br>
+            <br>
+            <div class="grid text-center">
+    <div class="row justify-content-auto">
+        <div class="card" style="width: 20rem;">
+            <img src="https://infoamazonia.org/wp-content/uploads/2016/02/aa-peru%CC%81-2011-2015.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                    <div class="card-body">
+                      <h5 class="card-title"> Tecnologo en Control Ambiental</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div><br>
+                    <br>
+                </div>
+                  <div class="card" style="width: 20rem;">
+                    <img src="https://universidadeuropea.com/resources/media/images/control-calidad-alimentos-1200x630.original.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                    <div class="card-body">
+                      <h5 class="card-title">Tecnologo en Control de Calidad de Alimentos</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                  <div class="card" style="width: 20rem;">
+                    <img src="https://www.traza.net/wp-content/uploads/2022/02/control_de_calidad_de_alimentos.jpg" class="card-img-top" alt="..." height="348" width="1000">
+                    <div class="card-body">
+                      <h5 class="card-title">Tecnologo en Gestión de Mercados</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
